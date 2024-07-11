e7e2774b9d69e1cfb64821cc3a9054f6bd4a06c4
Michael Krassowizkiy <mkrassowizkiy@outlook.de>
2024-07-11 11:48:49 +0200

    Open new dev branch for NNDL project.
    Adapted and extended ResNet model in models.py in accordance with
    torchvision's implementation (see https://pytorch.org/vision/main/_modules/torchvision/models/resnet.html).

--- models.py
+++ models.py
@@ -54,83 +54,176 @@ class ConvolutionalModel(nn.Module):
         output = self.net(x)
         return output
 
+# Resnet implementation inspired by https://pytorch.org/vision/main/_modules/torchvision/models/resnet.html
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
 
-class ResidualBlock(nn.Module):
+def conv1x1(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+
+# Basic residual block, used in shallower Resnets (resnet18 and resnet34)
+class BasicBlock(nn.Module):
+    expansion = 1
+
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
+        super(BasicBlock, self).__init__()
 
         # ConvBlock 1
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            conv3x3(in_channels, out_channels, stride),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU()
+            nn.ReLU(inplace=True)
         )
 
         # ConvBlock 2
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            conv3x3(out_channels, out_channels),
             nn.BatchNorm2d(out_channels)
         )
 
         self.downsample = downsample
-        self.relu = nn.ReLU()
-        self.out_channels = out_channels
+        self.relu = nn.ReLU(inplace=True)
+        self.out_channels = out_channels  # not actually used
+        self.stride = stride  # not actually used
 
     def forward(self, x):
         residual = x
+        
         out = self.conv1(x)
         out = self.conv2(out)
-        if self.downsample:
+        
+        if self.downsample is not None:
             residual = self.downsample(x)
+        
         out += residual
         out = self.relu(out)
+        
+        return out
+    
+# Residual block using Bottleneck architecture, used in deeper Resnets (from 50 layers upwards)
+class BottleneckBlock(nn.Module): # NOTE: stride applied at the 3x3 conv layer
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BottleneckBlock, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            conv1x1(in_channels, out_channels),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        self.conv2 = nn.Sequential( # NOTE: stride is applied here
+            conv3x3(out_channels, out_channels, stride),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        self.conv3 = nn.Sequential(
+            conv1x1(out_channels, self.expansion * out_channels),
+            nn.BatchNorm2d(self.expansion * out_channels)
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.out_channels = self.expansion * out_channels  # not actually used
+        self.stride = stride  # not actually used
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
         return out
 
-
+# Resnet implementation inspired by https://pytorch.org/vision/main/_modules/torchvision/models/resnet.html
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes):
         super(ResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU())
+        
+        self.inplanes = 64  # initial number of channel/feature expansion
+        
+        self.conv1 = nn.Sequential(  # initial downsampling blocks
+            nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.inplanes),
+            nn.ReLU(inplace=True)
+        )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # actual residual layers
         self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
         self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(2048, num_classes)
+        
+        # dense classification layer
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        # param initialization (same as in torchvision)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes:
+
+        if stride != 1 or self.inplanes != planes*block.expansion: # downsample when dimension of residual and out don't match
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes)
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion)
             )
+
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
-        for i in range(1, blocks):
+        layers.append(block(self.inplanes, planes, stride, downsample)) # downsampling done in first block
+        
+        self.inplanes = planes * block.expansion # update inplanes var
+        for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        
+        # preprocess
         x = self.conv1(x)
         x = self.maxpool(x)
+
+        # residual layers
         x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
 
+        # classfication via dense layer
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        x = torch.flatten(x, 1) # x.view(x.size(0), -1)
         x = self.fc(x)
 
         return x
+        
+# from https://github.com/facebookarchive/fb.resnet.torch/blob/master/models/resnet.lua
+resnet_cfg = {
+    "ResNet18" : {"block" : BasicBlock, "layers" : [2, 2, 2, 2]},
+    "ResNet34" : {"block" : BasicBlock, "layers" : [3, 4, 6, 3]},
+    "ResNet50" : {"block" : BottleneckBlock, "layers" : [3, 4, 6, 3]},
+    "ResNet101" : {"block" : BottleneckBlock, "layers" : [3, 4, 23, 3]},
+    "ResNet152" : {"block" : BottleneckBlock, "layers" : [3, 8, 36, 3]},
+}
+
+# example init: 
+#   resnet50 = ResNet(resnet_cfg["ResNet50"]["block"], resnet_cfg["ResNet50"]["layers"], num_classes=10)
 
 
 def train(train_loader: DataLoader, model: nn.Module, epoch: int, criterion: nn.modules.loss, optimizer: torch.optim,
